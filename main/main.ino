40043ca6cbb23ee72e186f028d3c3d100732c415
Arjun Parthasarathy <56085610+Dat-Boi-Arjun@users.noreply.github.com>
2023-02-02 13:28:50 -0800

    Code to read/write breakbeam

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,15 @@
+int BREAKBEAM_PIN = 53;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(BREAKBEAM_PIN, INPUT);
+  digitalWrite(BREAKBEAM_PIN, HIGH);
 
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println(digitalRead(BREAKBEAM_PIN));
+  delay(500);
 }
